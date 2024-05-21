4cc16db675e7a53ea12d79139f2a8b7220655ce8
fazleyazdan <fazleyazdan345@gmail.com>
2024-05-21 15:32:01 +0500

    pytest mark

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 import source.my_functions as my_functions
 
@@ -13,3 +14,28 @@ def test_division_by_zero():
     with pytest.raises(ZeroDivisionError):       
         result = my_functions.divide(5, 0)
     assert True              #! we are asserting 'True'. because by calling this function, the exception should be raised
+    
+    
+#* ********* Using Mark & Parameters ********** *#
+
+#* Mark is the same as 'tags' in cypress. i.e (skip, only)
+
+# if the test case execute slow then we can mark it as slow
+
+@pytest.mark.slow
+def test_add_slow():
+    time.sleep(5)
+    assert my_functions.add(3, 4) == 7
+    
+
+@pytest.mark.skip(reason= 'this test case already executed hence skipping')
+def test_add_skip():
+    assert my_functions.add(3, 4) == 7
+
+@pytest.mark.xfail(reason='we know that this test case is going to fail')      #! you can also add reason for explanation 
+def test_zero_division():
+    my_functions.divide(4,0)
+    
+# for skipped test case 's' & for xfail 'x' will be shown. in our case the first 4 cases passed, the fifth skipped and the last failed.
+#* in terminal it will be shown like this:    ....sx
+
