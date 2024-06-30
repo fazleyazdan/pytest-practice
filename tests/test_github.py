bfe75d22c3672b9617a1713ebd99f2c431492b6e
Fazle Yazdan <fazleyazdan345@gmail.com>
2024-06-30 23:15:52 +0500

    test for workflow

---
+++ tests/test_github.py
@@ -0,0 +1,38 @@
+
+import requests
+from munch import Munch
+
+def test_get_workflow():
+    
+    # Define the necessary variables
+    GITHUB_TOKEN = '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'
+    OWNER = 'fazleyazdan7'
+    REPO = 'github-api'
+    WORKFLOW_ID = 'main.yml' 
+
+    # end point
+    url = f"https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}"
+
+    # Headers
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {GITHUB_TOKEN}"
+    }
+
+    # Make a request
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    workflow_info = Munch(response.json())
+    
+    assert workflow_info.name == "hello-worldd"
+    assert workflow_info.state == "active"
+    
+    # Check the response status and print the workflow information
+    # if response.status_code == 200:
+    #     workflow_info = response.json()
+    #     print(workflow_info)
+    # else:
+    #     print(f"Failed to retrieve workflow information: {response.status_code}")
+    #     print(response.json())
+
---
+++ tests/test_trigger_workflow.py
@@ -0,0 +1,35 @@
+import requests
+from munch import Munch
+
+def test_get_workflow():
+    
+    # Define the necessary variables
+    GITHUB_TOKEN = 'Your token'
+    OWNER = 'fazleyazdan7'
+    REPO = 'github-api'
+    WORKFLOW_ID = 'main.yml'  
+
+    # headers
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {GITHUB_TOKEN}"
+    }
+    
+    trigger_url = f"https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}/dispatches"
+
+    # payload with the branch name
+    data = {
+        "ref": "main"  
+    }
+
+    # Make the POST request to trigger the workflow
+    response = requests.post(trigger_url, headers=headers, json=data)
+
+    # Check the response status 
+    assert response.status_code == 204
+    
+    # if response.status_code == 204:
+    #     print("Workflow triggered successfully!")
+    # else:
+    #     print(f"Failed to trigger workflow: {response.status_code}")
+    #     print(response.json())
