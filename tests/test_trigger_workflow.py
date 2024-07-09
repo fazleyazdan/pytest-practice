90ac305a6a987a6a945c617ea43dbe1e45b4d6b8
Fazle Yazdan <fazleyazdan345@gmail.com>
2024-07-09 13:40:31 +0500

    trigger  workflow

--- tests/test_trigger_workflow.py
+++ tests/test_trigger_workflow.py
@@ -28,4 +28,4 @@ def test_get_workflow():
     # Check the response status 
     assert response.status_code == 204
     
-#* status code '204' is returned when workflow is triggered
+#* status code '204' is returned when workflow is triggered.
